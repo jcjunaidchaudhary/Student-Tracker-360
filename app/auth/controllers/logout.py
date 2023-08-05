236bd8a1815620e9277d904829d7d47f36454026
Juned Chaudhary <87421043+jcjunaidchaudhary@users.noreply.github.com>
2023-08-05 19:14:01 +0530

    logout logic added

--- app/auth/controllers/logout.py
+++ app/auth/controllers/logout.py
@@ -1,35 +1,29 @@
-from flask import g, current_app, make_response
+from flask import current_app, jsonify, request
 from flask_restful import Resource
 
 from app.auth.decorators import require_login
 # from app.auth.session_manager import clear_session
 
+# Keep a blacklist of invalidated tokens
+token_blacklist = set()
 
 class LogOutView(Resource):
     method_decorators = [require_login]
 
     def get(self):
         """Remove the current authenticated user session from the session store.."""
+        token = request.cookies.get('currentUser')
+
+        if token:
+            # Add the token to the blacklist by setting its expiration to a past date
+            token_blacklist.add(token)
+            # Create a response to clear the cookie on the client-side
+            redirect_url = current_app.config.get("LOGOUT_URL", "http://localhost")
+
+            response = jsonify(message='Logged out successfully',redirect_url=redirect_url)
+            response.delete_cookie('currentUser')
+
+            return response
+
+        return jsonify(message='No token found'), 400
         
-        response = make_response("Cookie Cleared")
-
-        if hasattr(g, "user"):
-            response.set_cookie('cookie_name', '', expires=0)
-        
-
-        redirect_url = current_app.config.get("LOGOUT_URL", "http://localhost")
-
-        response = {"redirect_url": redirect_url}
-
-        return response, 200
-    
-    # def get(self):
-    #     """Remove the current authenticated user session from the session store.."""
-    #     if hasattr(g, "user"):
-    #         clear_session(g.user.id)
-
-    #     redirect_url = current_app.config.get("LOGOUT_URL", "http://localhost")
-
-    #     response = {"redirect_url": redirect_url}
-
-    #     return response, 200
